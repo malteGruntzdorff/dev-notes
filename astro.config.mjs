971cec68ad49696ad191f50a844f54383340d45e
Gruntzdorff, Malte <Malte.Gruntzdorff@adesso.de>
2025-11-20 10:38:37 +0100

    aktuell

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  // Für GitHub Pages später:
-  // site: "https://DEIN-GITHUB-USERNAME.github.io/DEIN-REPO-NAME/",
-  // base: "/DEIN-REPO-NAME/",
-  output: "static"
+ site: "https://maltegruntzdorff.github.io",
+ base: "/dev-notes/",
 });
